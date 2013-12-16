bc15ddfcfb84ccba13ac8fd33f34a927ad85b76e
Bartosz Dobrzycki <bartosz.dobrzycki@gmail.com>
2013-12-16 14:16:19 +0000

    Created thread02.cpp sample program.

---
+++ thread02.cpp
@@ -0,0 +1,24 @@
+// Compiled using GCC compiler (4.8.1) under Linux.
+// g++ -std=c++0x thread02.cpp -pthread -o a.out
+// Run: ./a.out
+
+#include <iostream>
+#include <thread>
+
+int main (int argc, char **argv)
+{
+  // Lambda - (from lambda calculus): define a function on the spot
+  // (inline anonymous function [] - read this symbol as Greek letter lambda)
+
+  // Fork-join parallelism
+  std::thread th([]()
+  {
+      std::cout << "Hello from thread!\n";
+  });  //< this symbol is characteristic for Lambda (this is the end of Lambda)
+
+  std::cout << "Hello World!\n";
+
+  th.join(); // Join worker thread to main thread. 
+
+  return 0;
+}
